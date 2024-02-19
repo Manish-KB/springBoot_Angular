264c31978b643cb531e6d49ebb22bb39c358131c
Manish K B <manish.kb@subex.com>
2024-02-19 09:33:16 +0530

    Added validation to update and add student page

--- src/app/add-student/add-student.component.ts
+++ src/app/add-student/add-student.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 import { Student } from '../student';
 import { StudentService } from '../student.service';
 import { Router } from '@angular/router';
-import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, Validators, FormBuilder, EmailValidator } from '@angular/forms';
+
 @Component({
   selector: 'app-add-student',
   templateUrl: './add-student.component.html',
@@ -11,17 +11,31 @@ import { ReactiveFormsModule } from '@angular/forms';
 })
 export class AddStudentComponent {
   student: Student = new Student();
+  form: FormGroup;
 
+
+constructor(private studentService: StudentService, private router: Router, private fb: FormBuilder) {
+  this.form = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    emailId: ['', [Validators.required, emailValidator]],
+  });
   
-  constructor(private studentService: StudentService, private router: Router) { }
-  onFormSubmit() {
-    this.studentService.addStudent(this.student).subscribe(data => {
-      console.log(data)
-    });
-    this.goToStudentList();
+  function emailValidator(control: AbstractControl): { [key: string]: any } | null {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const valid = emailPattern.test(control.value);
+    return valid ? null : { invalidEmail: true };
+  }
 
-  }
-  goToStudentList() {
-    this.router.navigate(['/students']);
-  }
+}
+onFormSubmit() {
+  this.studentService.addStudent(this.student).subscribe(data => {
+    console.log(data)
+  });
+  this.goToStudentList();
+
+}
+goToStudentList() {
+  this.router.navigate(['/students']);
+}
 }
--- src/app/update-student/update-student.component.ts
+++ src/app/update-student/update-student.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { StudentService } from '../student.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Student } from '../student';
+import { AbstractControl, FormControl, FormGroup, Validators, FormBuilder, EmailValidator } from '@angular/forms';
 
 @Component({
   selector: 'app-update-student',
@@ -11,8 +12,22 @@ import { Student } from '../student';
 export class UpdateStudentComponent implements OnInit {
   id: number;
   student = new Student();
+  form: FormGroup;
+  constructor(private studentService: StudentService, private route: ActivatedRoute, private router: Router, private fb: FormBuilder) {
+    this.form = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      emailId: ['', [Validators.required, emailValidator]],
+    });
 
-  constructor(private studentService: StudentService, private route: ActivatedRoute, private router: Router) { }
+    function emailValidator(control: AbstractControl): { [key: string]: any } | null {
+      const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      const valid = emailPattern.test(control.value);
+      return valid ? null : { invalidEmail: true };
+    }
+
+  }
+
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
@@ -23,12 +38,12 @@ export class UpdateStudentComponent implements OnInit {
 
 
   }
-  
+
   onFormSubmit() {
-    this.studentService.updateStudent(this.id,this.student).subscribe(data=>{
+    this.studentService.updateStudent(this.id, this.student).subscribe(data => {
       this.goToStudentList();
     })
-    
+
 
   }
   goToStudentList() {
